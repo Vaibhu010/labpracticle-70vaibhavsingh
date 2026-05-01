df9b7d0727c330e6fa60f12fe209679df31b245e
Vaibhu010 <145277101+Vaibhu010@users.noreply.github.com>
2026-05-01 13:55:58 +0530

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,8 @@
+const logger = (req, res, next) => {
+  const currentTime = new Date().toISOString();
+  console.log(`Request received at: ${currentTime}`);
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+module.exports = logger;
